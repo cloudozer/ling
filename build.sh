9524d547ec2ac4063dd7ba3a7125bbf3ded0fab2
Vladimir Kirillov <proger@wilab.org.ua>
2015-04-06 18:57:37 +0300

    introduce NETTLE_CFLAGS, ditch `nettle' part from build.sh
    - minor build.sh changes while here

--- build.sh
+++ build.sh
@@ -11,32 +11,12 @@ erlang() {
 	wget -c http://packages.erlang-solutions.com/debian/erlang_solutions.asc
 	sudo apt-key add erlang_solutions.asc
 	sudo apt-get update
-	sudo apt-get install -y --force-yes erlang git autoconf gcc-arm-none-eabi
+	sudo apt-get install -y --force-yes erlang git autoconf gcc-arm-none-eabi make
 }
 
 clone() {
-	# Get ling
-	git clone https://github.com/cloudozer/ling.git || true
-	cd ling && git checkout raspberry-pi
-}
-
-nettle() {
-	# Build and make nettle available for including and linking
-	cd core/lib/    # We do this here because the ling Makefile doesn't handle cross-compilation nicely yet.
-	wget -c https://ftp.gnu.org/gnu/nettle/nettle-2.7.1.tar.gz
-	tar zxf nettle-2.7.1.tar.gz
-	ln -s nettle-2.7.1 nettle # Makefile and includes expect nettle dir.
-	cd nettle 
-	./.bootstrap
-	if [ ARCH = "arm" ]; then
-		# newlib doesn't support assert very well, so set NDEBUG to elide asserts and prevent undefined ref errors at link time.
-		env CFLAGS='-DNDEBUG --specs=nosys.specs -mfloat-abi=hard -mfpu=vfp' ./configure --host=arm-none-eabi
-		make -j4 CROSS_COMPILE=$CROSS_COMPILE libnettle.a # All we need is libnettle.a and some headers.
-	else
-		./configure
-		make -j4 libnettle.a # All we need is libnettle.a and some headers.
-	fi
-	cd ../../..
+	git clone https://github.com/cloudozer/ling.git
+	cd ling
 }
 
 ling() {
@@ -50,10 +30,10 @@ ling() {
 
 
 case $1 in
-nettle)
-	nettle
+ling)
+	ling
 	;;
 *)
-	erlang; clone; nettle; ling
+	erlang; clone; ling
 	;;
 esac
--- core/Compiler.mk
+++ core/Compiler.mk
@@ -1,13 +1,17 @@
 
 ifeq ($(ARCH),arm)
 CC := arm-unknown-eabi-gcc
-NETTLE_FLAGS :=
 CPPFLAGS += -DLING_ARM
+
+NETTLE_FLAGS := --host=arm-none-eabi
+# newlib doesn't support assert very well, so set NDEBUG to elide asserts and prevent undefined ref errors at link time.
+NETTLE_CFLAGS := -DNDEBUG --specs=nosys.specs -mfloat-abi=hard -mfpu=vfp
 endif
 
 ifeq ($(ARCH),posix)
 CC := clang
 NETTLE_FLAGS :=
+NETTLE_CFLAGS :=
 CPPFLAGS += -DLING_POSIX
 
 # assuming Apple LLVM version 6.0 (clang-600.0.57)
@@ -24,6 +28,7 @@ CPPFLAGS += -D__XEN_INTERFACE_VERSION__=$(XEN_INTERFACE_VERSION)
 ifeq ($(shell uname),Darwin)
 CC := x86_64-pc-linux-gcc
 NETTLE_FLAGS := --host=x86_64-pc-linux
+NETTLE_CFLAGS :=
 else # Darwin
 CC := gcc
 NETTLE_FLAGS :=
