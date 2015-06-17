f5288f3acb1813d54a34b896d093db816ea18ba9
Maxim Kharchenko <maxim.kharchenko@gmail.com>
2015-06-17 14:35:55 +0300

    a3

--- core/heap_adapt.c
+++ core/heap_adapt.c
@@ -204,8 +204,13 @@ static int state_index(uint64_t now, int free_pages, region_t *root_regs, int nr
 //#define K3	(6e-1)
 
 //a2
-#define K1	(250000)
-#define K2	(2.5e-9)
+//#define K1	(250000)
+//#define K2	(2.5e-9)
+//#define K3	(6e-1)
+
+//a3
+#define K1	(1e6)
+#define K2	(1.25e-9)
 #define K3	(6e-1)
 
 static double calc_reward(int no_memory, uint32_t free_pages,
