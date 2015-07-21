b9793aa38f4dbcfc52e4f301cb7a26bdb98401fe
Maxim Kharchenko <maxim.kharchenko@gmail.com>
2015-07-21 06:19:44 +0300

    Refactores tube top side

--- apps/kernel/src/tube.erl
+++ apps/kernel/src/tube.erl
@@ -3,95 +3,14 @@
 -export([accept/0]).
 -export([close/1]).
 
--export([wait_peer/2]).
-
--include("tube.hrl").
-
 open(Domid) -> open(Domid, 0).
 open(Domid, Tid) when is_integer(Domid), is_integer(Tid) ->
-	Me = xenstore:domid(),
-	?g("Opening tube ~w from ~w to ~w\n", [Tid,Me,Domid]),
-	NockDir = lc(["/local/domain/",Me,"/data/nock/",Domid,"/",Tid]),
-	TipDir  = lc(["/local/domain/",Domid,"/data/tip/",Me,"/",Tid]),
-	case xenstore:read(NockDir) of
-		{ok,_} -> {error,exists};
-		_      -> case xenstore:mkdir(TipDir) of
-					ok -> open1(Domid, NockDir, TipDir);
-					_  -> {error,not_found} end end;
+	gen_server:call(tube_server, {open,Domid,Tid});
 open(_, _) -> {error,badarg}.
 
-open1(Domid, NockDir, TipDir) ->
-	%% TipDir exists, NockDir does not
-	ok = xenstore:mkdir(NockDir),
-	ok = xenstore:write(lc(NockDir, "/tip"), TipDir),
-	ok = xenstore:write(lc(NockDir, "/state"), ?STATE_INIT_WAIT),
-	?g("Nock state = InitWait, waking up the tip\n", []),
-	ok = xenstore:write(lc(TipDir, "/nock"), NockDir),	%% wake-up peer
-	TipState = lc(TipDir, "/state"),
-	case wait_peer(TipState, ?STATE_INITIALISED) of
-		{error,_} =Error -> %% peer gone while waiting for a tube to initialise
-			?g("Error while waiting for the tip to initialise\n", []),
-			ok = xenstore:delete(NockDir),
-			?g("Nock data deleted\n", []),
-			Error;
-		ok ->
-			{ok,RingRef} = xenstore:read_integer(lc(TipDir, "/ring-ref")),
-			{ok,ChanTx}  = xenstore:read_integer(lc(TipDir, "/event-channel-tx")),
-			{ok,ChanRx}  = xenstore:read_integer(lc(TipDir, "/event-channel-rx")),
+accept() -> gen_server:call(tube_server, accept, infinity).
 
-			?g("Tip: ring-ref = ~w\n", [RingRef]),
-			?g("Tip: event-channel-tx = ~w\n", [ChanTx]),
-			?g("Tip: event-channel-rx = ~w\n", [ChanRx]),
-
-			Tube = erlang:open_port(tube, ?TUBE_PORT_OPTS),
-			?g("Port ~w open\n", [Tube]),
-			Info = binary_to_list(<<Domid:32,RingRef:32,ChanTx:32,ChanRx:32>>),
-			[?TUBE_REP_OK] = erlang:port_control(Tube, ?TUBE_REQ_ATTACH, Info),
-			?g("Port ~w attached successfully\n", [Tube]),
-
-			ok = xenstore:write(lc(NockDir, "/state"), ?STATE_CONNECTED),
-			case wait_peer(TipState, ?STATE_CONNECTED) of
-				{error,_} =Error ->
-					?g("Error while waiting for the tip to connect\n", []),
-					true = unlink(Tube),
-					true = erlang:port_close(Tube),
-					?g("Port ~w closed\n", [Tube]),
-					ok = xenstore:delete(NockDir),
-					?g("Nock data destroyed\n", []),
-					Error;
-				ok ->
-					?g("Tip connected\n", []),
-					ok = tube_server:enlist(Tube, TipState, NockDir),
-					?g("Tube ~w enlisted with tube_server\n", [Tube]),
-					{ok,Tube} end end.
-
-accept() -> case gen_server:call(tube_server, accept, infinity) of
-				{ok,Tube} =X -> true = port_connect(Tube, self()), X;
-					Error -> Error end.
-					
-close(_Tube) -> throw(todo).
-
-%%state(?STATE_INITIALISING) -> "Initialising";
-%%state(?STATE_INIT_WAIT)	 -> "InitWait";
-%%state(?STATE_INITIALISED)  -> "Initialised";
-%%state(?STATE_CONNECTED)	 -> "Connected";
-%%state(?STATE_CLOSING)	     -> "Closing";
-%%state(?STATE_CLOSED)	     -> "Closed".
-
-wait_peer(Path, Target) ->
-	ok = xenstore:watch(Path),
-	Result = wait_peer1(Path, Target),
-	ok = xenstore:unwatch(Path),
-	Result.
-
-wait_peer1(Path, Target) ->
-	receive {watch,Path} ->
-		case xenstore:read(Path) of
-			{error,enoent} -> wait_peer1(Path, Target);		%% ignore
-			{ok,Target}	   -> ok;
-			{ok,_}		   -> wait_peer1(Path, Target);
-			{error,_} =Err -> Err end end.
-
-lc(X)    -> lists:concat(X).
-lc(X, Y) -> lists:concat([X,Y]).
+close(Tube) when is_port(Tube) ->
+	gen_server:close(tube_server, {close,Tube});
+close(_) -> {error,badarg}.
 
--- apps/kernel/src/tube_server.erl
+++ apps/kernel/src/tube_server.erl
@@ -10,8 +10,6 @@
 
 -export([start_link/0]).
 
--export([enlist/3]).
-
 %% ------------------------------------------------------------------
 %% gen_server Function Exports
 %% ------------------------------------------------------------------
@@ -26,9 +24,6 @@
 start_link() ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
-enlist(Tube, StatePath, DataDir) ->
-	gen_server:call(?SERVER, {enlist,Tube,StatePath,DataDir}).
-
 %% ------------------------------------------------------------------
 %% gen_server Function Definitions
 %% ------------------------------------------------------------------
@@ -38,38 +33,63 @@ enlist(Tube, StatePath, DataDir) ->
 init(_Args) ->
 	process_flag(trap_exit, true),
 	Me = xenstore:domid(),
+
 	TipTop = "data/tip",
 	ok = xenstore:mkdir(TipTop),
 	ok = xenstore:set_perms(TipTop, [lc("b", Me)]),
+
 	NockTop = "data/nock",
 	ok = xenstore:mkdir(NockTop),
 	ok = xenstore:set_perms(NockTop, [lc("r", Me)]),
+
 	xenstore:watch(TipTop),
 	{ok,#ts{top =TipTop}}.
 
-handle_call(accept, _From, #ts{pend =[Tube|Pend]} =St) ->
+handle_call({open,Domid,Tid}, {Caller,_} =_From, St) ->
+	Me = xenstore:domid(),
+	NockDir = lc(["/local/domain/",Me,"/data/nock/",Domid,"/",Tid]),
+	TipDir  = lc(["/local/domain/",Domid,"/data/tip/",Me,"/",Tid]),
+	case xenstore:read(NockDir) of
+		{ok,_} 	  -> {reply,{error,exists},St};
+		{error,_} ->
+			case xenstore:mkdir(TipDir) of
+				ok -> do_open(Domid, NockDir, TipDir, Caller, St);
+				_  -> {reply,{error,not_found},St} end end;
+
+handle_call(accept, {Caller,_} =_From, #ts{pend =[{Tube,MyState}|Pend]} =St) ->
+	port_connect(Tube, Caller),
+	ok = xenstore:write(MyState, ?STATE_CONNECTED),
 	{reply,{ok,Tube},St#ts{pend =Pend}};
 
 handle_call(accept, From, #ts{acc =Acc} =St) ->
 	{noreply,St#ts{acc =[From|Acc]}};
 
-handle_call({enlist,Tube,StatePath,DataDir}, _From, St) ->
-	St1 = do_enlist(Tube, StatePath, DataDir, St),
+handle_call({close,Tube}, _From, St) ->
+	St1 = close_tube(Tube, St),
 	{reply,ok,St1}.
 
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
-handle_info({watch,WatchKey}, St) ->
-	St1 = watch_event(WatchKey, St),
-    {noreply,St1};
+handle_info({watch,WatchKey}, #ts{top =TipTop} =St) ->
+	case lists:prefix(TipTop, WatchKey) of
+		true  -> Suffix = lists:nthtail(length(TipTop), WatchKey),
+				 case string:tokens(Suffix, "/") of
+					[X,Y,"nock"] ->
+						%% peer wants to communicate
+						{ok,NockDir} = xenstore:read(WatchKey),
+						Domid = list_to_integer(X),
+						knock_knock(Domid, NockDir, lc([TipTop,"/",X,"/",Y]), St);
+					_ -> {noreply,St} end;
+		false -> tube_state(WatchKey, St) end;
 
 handle_info(_Msg, St) ->
 	io:format("tube_server: msg ~p\n", [_Msg]),
 	{noreply,St}.
 
-terminate(_Reason, _State) ->
-    ok.
+terminate(_Reason, #ts{top =TipTop,tubes =Tubes}) ->
+	lists:foreach(fun({_,StatePath,_}) -> ok = xenstore:unwatch(StatePath) end, Tubes),
+	ok = xenstore:unwatch(TipTop).
 
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
@@ -78,57 +98,65 @@ code_change(_OldVsn, State, _Extra) ->
 %% Internal Function Definitions
 %% ------------------------------------------------------------------
 
-do_enlist(Tube, StatePath, DataDir, #ts{tubes =TI} =St) ->
-	ok = xenstore:watch(StatePath),
-	true = link(Tube),	%% catch exit signal
-	St#ts{tubes =[{Tube,StatePath,DataDir}|TI]}.
+do_open(Domid, NockDir, TipDir, Caller, #ts{tubes =Tubes} =St) ->
+	%% TipDir exists, NockDir does not
+	{ok,Tid} = xenstore:transaction(),
+	ok = xenstore:mkdir(NockDir, Tid),
+	ok = xenstore:write(lc(NockDir, "/tip"), TipDir, Tid),
+	ok = xenstore:write(lc(NockDir, "/state"), ?STATE_INIT_WAIT, Tid),
+	ok = xenstore:write(lc(TipDir, "/nock"), NockDir, Tid), %% wakes up peer
+	ok = xenstore:commit(Tid),
+	TipState = lc(TipDir, "/state"),
+	ok = xenstore:watch(TipState),
+	case wait_peer(TipState, ?STATE_INITIALISED) of
+		{error,_} =Error -> %% peer gone
+			ok = xenstore:delete(NockDir),
+			ok = xenstore:unwatch(TipState),
+			{reply,Error,St};
+		ok ->
+			{ok,RingRef} = xenstore:read_integer(lc(TipDir, "/ring-ref")),
+			{ok,ChanTx}  = xenstore:read_integer(lc(TipDir, "/event-channel-tx")),
+			{ok,ChanRx}  = xenstore:read_integer(lc(TipDir, "/event-channel-rx")),
+			Tube = erlang:open_port(tube, ?TUBE_PORT_OPTS),
+			Info = binary_to_list(<<Domid:32,RingRef:32,ChanTx:32,ChanRx:32>>),
+			[?TUBE_REP_OK] = erlang:port_control(Tube, ?TUBE_REQ_ATTACH, Info),
+			true = port_connect(Tube, Caller),
+			ok = xenstore:write(lc(NockDir, "/state"), ?STATE_CONNECTED),
+			case wait_peer(TipState, ?STATE_CONNECTED) of
+				{error,_} =Error ->
+					true = port_connect(Tube, self()), %% spare the caller
+					true = erlang:port_close(Tube),
+					ok = xenstore:delete(NockDir),
+					ok = xenstore:unwatch(TipState),
+					{reply,Error,St};
+				ok ->
+					%% TipState is being watched
+					TI = {Tube,TipState,NockDir},
+					{reply,{ok,Tube},St#ts{tubes = [TI|Tubes]}} end end.
 
-watch_event(WatchKey, #ts{top =TipTop, tubes =TI} =St) ->
-	case lists:prefix(TipTop, WatchKey) of
-		true ->
-			Suffix = lists:nthtail(length(TipTop), WatchKey),
-			case string:tokens(Suffix, "/") of
-				[X,Y,"nock"] ->
-					{ok,NockDir} = xenstore:read(WatchKey),
-					Domid = list_to_integer(X),
-					knock_knock(Domid, NockDir, lc([TipTop,"/",X,"/",Y]), St);
-				_ -> St end;
-		false ->
-			{value,{Tube,StatePath,DataDir},TI1} = lists:keytake(WatchKey, 2, TI),
-			case xenstore:read(StatePath) of
-				{ok,?STATE_CONNECTED} -> St;
-				{error,eacces} ->
-					ok = xenstore:unwatch(StatePath),
-					true = unlink(Tube),
-					true = port_close(Tube),
-					?g("Port ~w closed\n", [Tube]),
-					ok = xenstore:delete(DataDir),
-					?g("Tube data ~s deleted\n", [DataDir]),
-					Pend1 = lists:delete(Tube, St#ts.pend),
-					St#ts{pend =Pend1,tubes =TI1} end end.
+wait_peer(Path, Target) ->
+	receive {watch,Path} ->
+		case xenstore:read(Path) of
+			{error,enoent} -> wait_peer(Path, Target);		%% ignore
+			{ok,Target}	   -> ok;
+			{ok,_}		   -> wait_peer(Path, Target);
+			{error,_} =Err -> Err end end.
 
-knock_knock(Domid, NockDir, TipDir, St) ->
-	?g("Accepting tube request from ~w, nock = ~s\n", [Domid,NockDir]),
+knock_knock(Domid, NockDir, TipDir, #ts{tubes =Tubes} =St) ->
 	NockState = lc(NockDir, "/state"),
+	ok = xenstore:watch(NockState),
 	TipState = lc(TipDir, "/state"),
 	ok = xenstore:write(TipState, ?STATE_INITIALISING),
-	?g("Tip state = Initialising\n", []),
-	case tube:wait_peer(NockState, ?STATE_INIT_WAIT) of
+	case wait_peer(NockState, ?STATE_INIT_WAIT) of
 		{error,_} ->
-			?g("Error while waiting for the nock (to start waiting)\n", []),
 			ok = xenstore:delete(TipDir),
-			?g("Tip data deleted\n", []),
-			St;
+			ok = xenstore:unwatch(NockState),
+			{noreply,St};
 		ok ->
 			Tube = erlang:open_port(tube, ?TUBE_PORT_OPTS),
-			?g("Port ~w open\n", [Tube]),
 			Info = binary_to_list(<<Domid:32>>),
 			[?TUBE_REP_OK|Data] = erlang:port_control(Tube, ?TUBE_REQ_OPEN, Info),
 			<<RingRef:32,ChanTx:32,ChanRx:32>> = list_to_binary(Data),
-
-			?g("ring-ref = ~w\n", [RingRef]),
-			?g("event-channel-tx = ~w\n", [ChanTx]),
-			?g("event-channel-rx = ~w\n", [ChanRx]),
 
 			{ok,Tid} = xenstore:transaction(),
 			ok = xenstore:write(lc(TipDir, "/ring-ref"), RingRef, Tid),
@@ -136,32 +164,35 @@ knock_knock(Domid, NockDir, TipDir, St) ->
 			ok = xenstore:write(lc(TipDir, "/event-channel-rx"), ChanRx, Tid),
 			ok = xenstore:write(TipState, ?STATE_INITIALISED, Tid),
 			ok = xenstore:commit(Tid),
-			?g("Tip state = Initialised\n", []),
+			ok = wait_peer(NockState, ?STATE_CONNECTED),
 
-			case tube:wait_peer(NockState, ?STATE_CONNECTED) of
-				{error,_} ->
-					?g("Error while waiting for the nock to connect\n", []),
-					true = unlink(Tube),
-					true = erlang:port_close(Tube),
-					?g("Port ~w closed\n", [Tube]),
-					ok = xenstore:delete(TipDir),
-					?g("Tip data destroyed\n", []),
-					St;
-				ok ->
-					?g("Nock connected\n", []),
+			TI = {Tube,NockState,TipDir},
+			St1 = St#ts{tubes = [TI|Tubes]},
+
+			case St1#ts.acc of
+				[{Caller,_} =From|Acc1] ->
+					port_connect(Tube, Caller),
 					ok = xenstore:write(TipState, ?STATE_CONNECTED),
-					?g("Tip state = Connected\n", []),
-					St1 = do_enlist(Tube, NockState, TipDir, St),
-					?g("Tube enlisted\n", []),
-					accepted(Tube, St1) end end.
+					gen_server:reply(From, {ok,Tube}),
+					{noreply,St1#ts{acc =Acc1}};
+				[] ->
+					%% Tube not connected (temporarily connected to tube_server)
+					{noreply,St1#ts{pend =[{Tube,TipState}|St#ts.pend]}} end end.
 
-accepted(Tube, #ts{acc =[From|Acc]} =St) ->
-	?g("Tube ~w accepted by ~w\n", [Tube,From]),
-	gen_server:reply(From, {ok,Tube}),
-	St#ts{acc =Acc};
+tube_state(WatchKey, #ts{tubes =Tubes} =St) ->
+	{Tube,StatePath,_} = lists:keyfind(WatchKey, 2, Tubes),
+	case xenstore:read(StatePath) of
+		{ok,?STATE_CONNECTED} -> {noreply,St};
+		_ -> St1 = close_tube(Tube, St), {noreply,St1} end.
 
-accepted(Tube, #ts{pend =Pend} =St) ->
-	St#ts{pend =[Tube|Pend]}.
+close_tube(Tube, #ts{tubes =Tubes} =St) ->
+	{value,{_,StatePath,DataDir},Tubes1} = lists:keytake(Tube, 1, Tubes),
+	ok = xenstore:unwatch(StatePath),
+	true = unlink(Tube),
+	true = port_close(Tube),
+	ok = xenstore:delete(DataDir),
+	Pend1 = lists:delete(Tube, St#ts.pend),
+	St#ts{pend =Pend1,tubes =Tubes1}.
 
 lc(X) -> lists:concat(X).
 lc(X, Y) -> lists:concat([X,Y]).
