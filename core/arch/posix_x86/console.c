52cf53ef0cac0ecfa1e4458c335742ed460c2590
Dmytro S <dmytrish@gmail.com>
2015-07-30 19:48:54 +0300

    fixed POSIX build without LING_DEBUG

--- core/arch/posix_x86/console.c
+++ core/arch/posix_x86/console.c
@@ -73,6 +73,8 @@ console_do_pending(void)
 	return total;
 }
 
+#if LING_DEBUG
+
 #include <stdarg.h>
 
 int debug(const char *fmt, ...) {
@@ -82,3 +84,4 @@ int debug(const char *fmt, ...) {
     va_end(ap);
     return ret;
 }
+#endif
